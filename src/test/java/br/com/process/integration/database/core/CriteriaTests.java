eaa00d7fe4324b2039379a37f0087c0370574122
Anderson Nascimento <anderltda@gmail.com>
2025-04-22 18:46:12 -0300

    ajusts

--- src/test/java/br/com/process/integration/database/core/CriteriaTests.java
+++ src/test/java/br/com/process/integration/database/core/CriteriaTests.java
@@ -1,8 +1,8 @@
 package br.com.process.integration.database.core;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
--- src/test/java/br/com/process/integration/database/core/SaveInsertsFlushTests.java
+++ src/test/java/br/com/process/integration/database/core/SaveInsertsFlushTests.java
@@ -1,6 +1,6 @@
 package br.com.process.integration.database.core;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
