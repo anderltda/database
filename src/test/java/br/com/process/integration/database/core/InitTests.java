8a684e0f9ce7c2dc6048c2e3c5b108f1770008ce
Anderson Nascimento <anderltda@gmail.com>
2025-05-07 18:28:16 -0300

    ajusts

--- src/test/java/br/com/process/integration/database/core/InitTests.java
+++ src/test/java/br/com/process/integration/database/core/InitTests.java
@@ -1,14 +1,27 @@
 package br.com.process.integration.database.core;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.nio.charset.StandardCharsets;
 import java.sql.Connection;
+import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -20,13 +33,51 @@ import org.springframework.test.context.ActiveProfiles;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class InitTests {
 
+	@Autowired
+	private DataSource dataSource;
+	
+	private Map<String, Boolean> mapTables = new HashMap<>();
+
+	private List<String> tables = Arrays.asList("ENTITY_STATUS", "ENTITY_NINE", "ENTITY_EIGHT", "ENTITY_SEVEN", "ENTITY_SIX", "ENTITY_FIVE", "ENTITY_FOUR", "ENTITY_TREE", "ENTITY_TWO", "ENTITY_ONE");
+
 	@BeforeAll
 	void initDatabase() throws Exception {
-	    Resource resource = new ClassPathResource("sql/test-schema.sql");
-	    String sql = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
-	    try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "")) {
-	        Statement stmt = conn.createStatement();
-	        stmt.execute(sql);
-	    }
+		
+		tables.forEach(table -> {
+			mapTables.put(table, false);
+		});
+		
+		Resource resource = new ClassPathResource("sql/test-schema.sql");
+		String sql = new String(resource.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+		try (Connection conn = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "")) {
+			Statement stmt = conn.createStatement();
+			stmt.execute(sql);
+		}
+	}
+
+	@Test
+	@Order(1)
+	void listarTabelas() throws Exception {
+		try (Connection conn = dataSource.getConnection()) {
+			DatabaseMetaData metaData = conn.getMetaData();
+			ResultSet tables = metaData.getTables(null, null, "%", new String[] { "TABLE" });
+			while (tables.next()) {
+				String table = tables.getString("TABLE_NAME");
+				putTrueTable(table);
+			}
+			
+			mapTables.forEach((tableName, exists) -> {
+			    assertTrue(exists);
+			});
+		}
+	}
+
+	private void putTrueTable(String table) {
+
+		boolean existe = tables.stream().anyMatch(t -> t.equalsIgnoreCase(table));
+
+		if (existe) {
+			mapTables.put(table, true);
+		} 
 	}
 }
