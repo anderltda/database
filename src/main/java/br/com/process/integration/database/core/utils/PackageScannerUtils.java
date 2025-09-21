6de7bcab287642554dab0c875c176cd66401da65
Anderson Nascimento <anderltda@gmail.com>
2025-09-20 22:16:33 -0300

    ajusts

--- src/main/java/br/com/process/integration/database/core/utils/PackageScannerUtils.java
+++ src/main/java/br/com/process/integration/database/core/utils/PackageScannerUtils.java
@@ -1,7 +1,7 @@
 package br.com.process.integration.database.core.utils;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -23,7 +23,7 @@ public class PackageScannerUtils {
 	public static Object findClassBySimpleName(String basePackage, String className) throws CheckedException {
 		try {
 			Class<?> clazz = findByClass(basePackage, className);
-			if(clazz != null) {
+			if (clazz != null) {
 				return clazz.getDeclaredConstructor().newInstance();
 			}
 			throw new CheckedException(String.format("Class not found %s !", className));
@@ -34,68 +34,16 @@ public class PackageScannerUtils {
 
 	/**
 	 * @param basePackage
-	 * @param className
+	 * @param classNameFound
 	 * @return
 	 * @throws Exception
 	 */
-	@Deprecated
-	public static String findClassPackageByName(String basePackage, String className) throws Exception {
-		Set<Class<?>> classes = findAllClasses(basePackage);
-		for (Class<?> clazz : classes) {
-			if (clazz.getSimpleName().equalsIgnoreCase(className)) {
-				return clazz.getPackageName();
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Lista todas as classes em tempo de execução dentro de um pacote.
-	 *
-	 * @param basePackage Ex: "br.com.enginer.domain"
-	 * @return Set<Class<?>> com todas as classes encontradas
-	 * @throws Exception
-	 */
-	private static Set<Class<?>> findAllClasses(String basePackage) throws Exception {
-		Set<Class<?>> classes = new HashSet<>();
-		try {
-			String className = null;
-			String path = ClassUtils.convertClassNameToResourcePath(basePackage);
-			String classPattern = "classpath*:" + path + "/**/*.class";
-
-			PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-			Resource[] resources = resolver.getResources(classPattern);
-
-			for (Resource resource : resources) {
-				String resourcePath = resource.getURL().toString();
-				if (resourcePath.contains("/BOOT-INF/classes/")) {
-					className = resourcePath
-							.substring(resourcePath.indexOf("/BOOT-INF/classes/") + 18)
-							.replace("/", ".")
-							.replace(".class", "");
-				} else if (resourcePath.contains("/classes/")) {
-					className = resourcePath
-							.substring(resourcePath.indexOf("/classes/") + 9)
-							.replace("/", ".")
-							.replace(".class", "");
-				}
-				Class<?> clazz = Class.forName(className);
-				classes.add(clazz);
-			}
-
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			throw ex;
-		}
-		return classes;
-	}
-	
 	private static Class<?> findByClass(String basePackage, String classNameFound) throws Exception {
 		Class<?> clazz = null;
 		try {
 			String className = null;
 			String path = ClassUtils.convertClassNameToResourcePath(basePackage);
-			String classPattern = "classpath*:" + path + "/**/*.class";
+			String classPattern = "classpath*:" + path + "/**/**.class";
 
 			PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
 			Resource[] resources = resolver.getResources(classPattern);
@@ -103,20 +51,16 @@ public class PackageScannerUtils {
 			for (Resource resource : resources) {
 				String resourcePath = resource.getURL().toString();
 				if (resourcePath.contains("/BOOT-INF/classes/")) {
-					className = resourcePath
-							.substring(resourcePath.indexOf("/BOOT-INF/classes/") + 18)
-							.replace("/", ".")
-							.replace(".class", "");
+					className = resourcePath.substring(resourcePath.indexOf("/BOOT-INF/classes/") + 18)
+							.replace("/", ".").replace(".class", "");
 				} else if (resourcePath.contains("/classes/")) {
-					className = resourcePath
-							.substring(resourcePath.indexOf("/classes/") + 9)
-							.replace("/", ".")
+					className = resourcePath.substring(resourcePath.indexOf("/classes/") + 9).replace("/", ".")
 							.replace(".class", "");
 				}
 				
-				clazz = Class.forName(className);
-				
-				if(clazz.getSimpleName().equals(StringsUtils.firstUpper(classNameFound))) {
+				clazz = Class.forName(sanitizeDirectoryPath(className));
+
+				if (clazz.getSimpleName().equals(StringsUtils.firstUpper(classNameFound))) {
 					clazz = Class.forName(clazz.getPackageName() + "." + StringsUtils.firstUpper(classNameFound));
 					return clazz;
 				}
@@ -126,7 +70,27 @@ public class PackageScannerUtils {
 			ex.printStackTrace();
 			throw ex;
 		}
-		
+
 		return null;
 	}
+
+	/**
+	 * @param path
+	 * @return
+	 */
+	private static String sanitizeDirectoryPath(String path) {
+		String[] parts = path.split("/");
+		// Regex: permite letras, números, underscore e hífen
+		String safeRegex = "[^a-zA-Z0-9._-]";
+		List<String> sanitizedParts = new ArrayList<>();
+		for (String part : parts) {
+			// Remove caracteres inválidos de cada "pasta" ou arquivo
+			String sanitized = part.replaceAll(safeRegex, "");
+			if (!sanitized.isBlank()) {
+				sanitizedParts.add(sanitized);
+			}
+		}
+		return String.join("/", sanitizedParts);
+	}
+
 }
